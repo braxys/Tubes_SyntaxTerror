a740e4f4a222af8db5ff729afd259b812bea5e1c
Aryaaa <arya.124140212@student.itera.ac.id>
2026-05-12 19:45:30 +0700

    feat: implement greedy strategy based on ramming

--- src/alternative-bots/alt-bot-3/Rhino.cs
+++ src/alternative-bots/alt-bot-3/Rhino.cs
@@ -4,31 +4,37 @@ using System.Drawing;
 
 public class Rhino : Bot
 {
-    // The main method starts our bot
-    static void Main(string[] args)
-    {
-        new Rhino().Start();
-    }
-
-    // Constructor, which loads the bot config file
+    static void Main(string[] args) { new Rhino().Start(); }
     Rhino() : base(BotInfo.FromFile("Rhino.json")) { }
 
-    // Called when a new round is started -> initialize and do some movement
     public override void Run()
     {
+        BodyColor   = Color.FromArgb(0x70, 0x70, 0x70); 
+        TurretColor = Color.FromArgb(0x3A, 0x3A, 0x3A); 
+        RadarColor  = Color.FromArgb(0xFF, 0x66, 0x00); 
+        BulletColor = Color.FromArgb(0xFF, 0x22, 0x22); 
+        ScanColor   = Color.FromArgb(0xFF, 0xAA, 0x00); 
+        TracksColor = Color.FromArgb(0x10, 0x10, 0x10); 
+        GunColor    = Color.FromArgb(0x99, 0x99, 0x99); 
 
-        BodyColor   = Color.FromArgb(0x70, 0x70, 0x70); // Steel Gray
-        TurretColor = Color.FromArgb(0x3A, 0x3A, 0x3A); // Gunmetal
-        RadarColor  = Color.FromArgb(0xFF, 0x66, 0x00); // Aggressive Orange
-        BulletColor = Color.FromArgb(0xFF, 0x22, 0x22); // Crimson Red
-        ScanColor   = Color.FromArgb(0xFF, 0xAA, 0x00); // Amber
-        TracksColor = Color.FromArgb(0x10, 0x10, 0x10); // Black
-        GunColor    = Color.FromArgb(0x99, 0x99, 0x99); // Metallic Silver
-
-        // Repeat while the bot is running
         while (IsRunning)
         {
-            // Write your bot logic
+            TurnRadarLeft(360);
+        }
+    }
+
+    public override void OnScannedBot(ScannedBotEvent e)
+    {
+        double targetDir = DirectionTo(e.X, e.Y);
+        double distance = DistanceTo(e.X, e.Y);
+
+        TurnRight(CalcBearing(targetDir));
+        Forward(distance);
+
+        TurnGunRight(CalcDeltaAngle(GunDirection, targetDir));
+        if (distance < 100)
+        {
+            Fire(3); 
         }
     }
 }
